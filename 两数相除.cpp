ed1c744a4d8e70c3cc769a46565a526ba5823d2c
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-10-12 20:53:01 +0800

    Create 两数相除.cpp

---
+++ 两数相除.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    // quick multiply: a * b
+    int qm(int a, int b) {
+        int ans = 0;
+        while (b) {
+            if (b & 1) {
+                ans += a;
+            }
+            b >>= 1;
+            a <<= 1;
+        }
+        return ans;
+    }
+    
+    int divide(int dividend, int divisor) {
+        // special judge
+        if (abs(dividend) < abs(divisor)) {
+            return 0;
+        }
+        bool is_minus_dividend = dividend < 0 ? true : false;
+        bool is_minus_divisor = divisor < 0 ? true : false;
+        if (is_minus_dividend) {
+            dividend = -dividend;
+        }
+        if (is_minus_divisor) {
+            divisor = -divisor;
+        }
+        bool is_ans_positive = (is_minus_dividend && is_minus_divisor) || (!is_minus_dividend && !is_minus_divisor);
+        int i = 1, j = dividend;
+        while (i <= j) {
+            int mid = (i + j) >> 1;
+            int ans = qm(mid, divisor);
+            if (ans == dividend) {
+                // return mid;
+                j = mid;
+                break;
+            }
+            else if (ans < dividend) {
+                i = mid + 1;
+            }
+            else if (ans > dividend) {
+                j = mid - 1;
+            }
+        }
+        return is_ans_positive ? j : -j; 
+    }
+};
