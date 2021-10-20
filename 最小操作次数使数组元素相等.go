c6080362e7cf156642213fe0c30c4945ed91ac39
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-10-20 19:30:48 +0800

    Update 最小操作次数使数组元素相等.go

--- 最小操作次数使数组元素相等.go
+++ 最小操作次数使数组元素相等.go
@@ -1,49 +1,24 @@
-func maxNum(nums []int) (int, int) {
-    len := len(nums)
-    max := nums[0]
-    id := 0
-    for i := 1; i < len; i += 1 {
-        if nums[i] > max {
-            max = nums[i]
-            id = i
-        }
-    }
-    return max, id
-}
-
-func minNum(nums []int) (int, int) {
+func minNum(nums []int) int {
     len := len(nums)
     min := nums[0]
-    id := 0
     for i := 1; i < len; i += 1 {
         if nums[i] < min {
             min = nums[i]
-            id = i
         }
     }
-    return min, id
+    return min
 }
 
-func increment(nums []int, exclude_id int, delta int) {
-    len := len(nums)
-    for i := 0; i < len; i += 1 {
-        if i != exclude_id {
-            nums[i] += delta
-        }
+func sumDistance(nums []int, min int) int {
+    sum := 0
+    for _, x := range nums {
+        sum += (x - min)
     }
+    return sum
 }
 
 func minMoves(nums []int) int {
-    cnt := 0
-    max := 0
-    min := 0
-    max_id := 0
-    min_id := 1  // differ at first init
-    for max_id != min_id {
-        max, max_id = maxNum(nums)
-        min, min_id = minNum(nums)
-        cnt += (max - min)
-        increment(nums, max_id, max - min)
-    }  
-    return cnt
+    min := minNum(nums)
+    ans := sumDistance(nums, min)
+    return ans
 }
