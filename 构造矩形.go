1b09f46d6bdb9a10221936da83498eb039c4a78f
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-10-23 21:36:55 +0800

    Create 构造矩形.go

---
+++ 构造矩形.go
@@ -0,0 +1,12 @@
+func constructRectangle(area int) []int {
+    w := int(math.Sqrt(float64(area)))
+    arr := make([]int, 2)
+    for ; w >= 1; w -= 1 {
+        if (area % w) == 0 {
+            arr[0] = area / w
+            arr[1] = w
+            break
+        }
+    }
+    return arr
+}
