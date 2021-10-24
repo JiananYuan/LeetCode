7146635cf309c070be4624c7b9381598a189bb66
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-10-24 22:08:57 +0800

    Create 大礼包.cpp

---
+++ 大礼包.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    map<vector<int>, int> mp;
+
+    int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
+        return dfs(price, special, needs);
+    }
+
+    int dfs(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
+        if (mp.count(needs) == 1) {
+            return mp[needs];
+        }
+        int ans = 0;
+        int needs_len = int(needs.size());
+        for (int i = 0; i < needs_len; i += 1) {
+            ans += needs[i] * price[i];
+        }
+        int special_len = int(special.size());
+        for (int i = 0; i < special_len; i += 1) {
+            vector<int> next(needs);
+            bool isOverflow = false;
+            int special_item_len = int(special[i].size());
+            for (int j = 0; j < special_item_len; j += 1) {
+                if (next[i] < special[i][j]) {
+                    isOverflow = true;
+                    break;
+                }
+            }
+            if (!isOverflow) {
+                for (int j = 0; j < needs_len; j += 1) {
+                    next[i] -= special[i][j];
+                }
+                ans = min(ans, dfs(price, special, next) + special[i].back());
+            }
+        }
+        return mp[needs] = ans;
+    }
+};
