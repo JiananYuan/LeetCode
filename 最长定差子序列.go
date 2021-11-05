864cdbbe2ae70adc4b553b8c5628066423db495d
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-11-05 12:13:15 +0800

    Create 最长定差子序列.go

---
+++ 最长定差子序列.go
@@ -0,0 +1,30 @@
+func longestSubsequence(arr []int, difference int) int {
+	mark := map[int]bool{}
+	len := len(arr)
+	maxLength := 0
+	for i := 0; i < len; i += 1 {
+		if mark[ arr[i] ] == true {
+			continue
+		}
+		mark[ arr[i] ] = true
+		tmpLength := 1
+		lastNum := arr[i]
+		for j := i + 1; j < len; j += 1 {
+			if arr[j] - lastNum	== difference {
+				tmpLength += 1
+				mark[ arr[j] ] = true
+                lastNum	= arr[j]
+			}
+		}
+		maxLength = max(maxLength, tmpLength)
+	}
+	return maxLength
+}
+
+func max(a int, b int) int  {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
