73a7ca8058afdf73ecf841fd7b5116105df0d0e3
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-11-07 11:14:15 +0800

    Create 范围求和.go

---
+++ 范围求和.go
@@ -0,0 +1,21 @@
+func maxCount(m int, n int, ops [][]int) int {
+    minx := 0x7fffffff
+    miny := minx
+    lenx := len(ops)
+    if lenx == 0 {
+        return m * n
+    }
+    for i := 0; i < lenx; i += 1 {
+        minx = min(minx, ops[i][0])
+        miny = min(miny, ops[i][1])
+    }
+    return minx * miny
+}
+
+func min(a int, b int) int {
+    if a < b {
+        return a
+    } else {
+        return b
+    }
+}
