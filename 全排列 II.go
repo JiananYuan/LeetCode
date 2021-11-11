c9eaf4b13af8929ab25c431c9dc076a4645a08ba
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-11-11 09:04:35 +0800

    Update 全排列 II.go

--- 全排列 II.go
+++ 全排列 II.go
@@ -44,3 +44,37 @@ func isSame(a []int, b []int) bool {
 	}
 	return true
 }
+
+
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        set<vector<int> > anss;
+        int length = int(nums.size());
+        bool visit[10] = {0};
+        vector<int> current;
+        solve(nums, length, visit, current, anss);
+        vector<vector<int> > ans;
+        for (auto it = nums.begin(); it != nums.end(); it++) {
+            ans.emplace_back(*it);
+        }
+        return ans;
+    }
+
+    void solve(vector<int>& nums, int& length, bool visit[], vector<int>& current, set<vector<int> >& ans) {
+        if (int(current.size()) == length) {
+            ans.insert(current);
+            return;
+        }
+        for (int i = 0; i < length; i += 1) {
+            if (visit[i] == false) {
+                visit[i] = true;
+                current.push_back(nums[i]);
+                solve(nums, length, visit, current, ans);
+                current.pop_back();
+                visit[i] = false;
+            }
+        }
+    }
+
+};
