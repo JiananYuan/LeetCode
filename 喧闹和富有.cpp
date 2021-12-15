a24500252e57506bf00353c8b73f21d506566219
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-12-15 12:41:58 +0800

    Update 喧闹和富有.cpp

--- 喧闹和富有.cpp
+++ 喧闹和富有.cpp
@@ -12,19 +12,28 @@ struct node {
 class Solution {
 public:
 
-    bool cmp(node& a, node& b) {
+    vector<vector<int> > ranks(520);
+
+    static bool cmp(node& a, node& b) {
         return a.richer_rank < b.richer_rank;
     }
 
     vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+        ranks.clear();
         int richer_len = int(richer.size());
         int quiet_len = int(quiet.size());
+        // 初始化富豪榜
         vector<node> richer_rank;
         for (int i = 0; i < quiet_len; i += 1) {
             richer_rank.emplace_back(node(i, 0, quiet[i]));
         }
+        // 拉链法求名次
         for (int i = 0; i < richer_len; i += 1) {
-            richer_rank[ richer[i][1] ] += 1;
+            ranks[ richer[i][0] ].push_back(richer[i][1]);
+        }
+        // to be continued...
+        for (int i = 0; i < richer_len; i += 1) {
+            richer_rank[ richer[i][1] ].richer_rank += 1;
         }
         sort(richer_rank.begin(), richer_rank.end(), cmp);
         vector<int> ans;
