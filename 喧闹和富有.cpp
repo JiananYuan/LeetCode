c93ea0fae03d31a271160d475732b8ff857a2836
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-12-15 12:29:22 +0800

    Create 喧闹和富有.cpp

---
+++ 喧闹和富有.cpp
@@ -0,0 +1,44 @@
+struct node {
+    int id;
+    int richer_rank;
+    int quiet;
+    node(int _id = 0, int _richer_rank = 0, int _quiet = 0) {
+        id = _id;
+        richer_rank = _richer_rank;
+        quiet = _quiet;
+    }
+};
+
+class Solution {
+public:
+
+    bool cmp(node& a, node& b) {
+        return a.richer_rank < b.richer_rank;
+    }
+
+    vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+        int richer_len = int(richer.size());
+        int quiet_len = int(quiet.size());
+        vector<node> richer_rank;
+        for (int i = 0; i < quiet_len; i += 1) {
+            richer_rank.emplace_back(node(i, 0, quiet[i]));
+        }
+        for (int i = 0; i < richer_len; i += 1) {
+            richer_rank[ richer[i][1] ] += 1;
+        }
+        sort(richer_rank.begin(), richer_rank.end(), cmp);
+        vector<int> ans;
+        for (int i = 0; i < quiet_len; i += 1) {
+            int most_quiet = 1000;
+            int most_quiet_id = i;
+            for (int j = i - 1; j >= 0; j -= 1) {
+                if (most_quiet > richer_rank[j].quiet) {
+                    most_quiet = richer_rank[j].quiet;
+                    most_quiet_id = richer_rank[j].id;
+                }
+            }
+            ans.push_back(most_quiet_id);
+        }
+        return ans;
+    }
+};
