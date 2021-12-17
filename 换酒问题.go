2ad8f5294c8ad6fbab98b654c5eb3c10c2de57d4
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-12-17 11:23:00 +0800

    Create 换酒问题.go

---
+++ 换酒问题.go
@@ -0,0 +1,9 @@
+func numWaterBottles(numBottles int, numExchange int) int {
+    cnt := numBottles
+    empty := numBottles
+    for empty >= numExchange {
+        cnt += (empty / numExchange)
+        empty = empty / numExchange + empty % numExchange
+    }
+    return cnt
+}
