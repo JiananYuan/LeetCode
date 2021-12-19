3990c172e5d74e7e0c1369032f3354fc8f52903f
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-12-19 16:59:33 +0800

    Create H指数.cpp

---
+++ H指数.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int citations_len = int(citations.size());
+        int h_index = -1;
+        for (int i = 0; i < citations_len; i += 1) {
+            h_index = max(h_index, min(citations[i], citations_len - i));
+        }
+        return h_index;
+    }
+};
