fa49a60c54d5252056e9ffdd2e8de036456fd5ed
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-12-20 12:46:02 +0800

    Create 找出数组中的第一个回文字符串.cpp

---
+++ 找出数组中的第一个回文字符串.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        int len = int(words.size());
+        for (int i = 0; i < len; i += 1) {
+            if (isHuiwen(words[i])) {
+                return words[i];
+            }
+        }
+        return "";
+    }
+    
+    inline bool isHuiwen(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return false;
+            }
+            i += 1;
+            j -= 1;
+        }
+        return true;
+    }
+};
