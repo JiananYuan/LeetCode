2660672276788c1e963112ce8bc3ea938546e498
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-12-26 09:08:08 +0800

    Create Bigram分词.cpp

---
+++ Bigram分词.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> findOcurrences(string text, string first, string second) {
+        vector<string> str_vec;
+        string tmp;
+        stringstream ss(text);
+        while (ss >> tmp) {
+            str_vec.emplace_back(tmp);
+        }
+        int sz = int(str_vec.size());
+        vector<string> ans;
+        for (int i = 0; i < sz; i += 1) {
+            if (str_vec[i] == first && i + 1 < sz && str_vec[i + 1] == second && i + 2 < sz) {
+                ans.emplace_back(str_vec[i + 2]);
+            }
+        }
+        return ans;
+    }
+};
