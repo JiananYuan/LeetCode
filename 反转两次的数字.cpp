353ff2fc9632a05b970fb76d5102a9b37760f8fc
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-12-27 16:33:17 +0800

    Create 反转两次的数字.cpp

---
+++ 反转两次的数字.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        int r1 = 0;
+        int _num = num;
+        while (_num != 0) {
+            r1 = r1 * 10 + _num % 10;
+            _num /= 10;
+        }
+        int r2 = 0;
+        while (r1 != 0) {
+            r2 = r2 * 10 + r1 % 10;
+            r1 /= 10;
+        }
+        return r2 == num;
+    }
+};
