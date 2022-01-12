100d21baa340d697f6d90634e88ac9bd403d8d9f
Andy <53621620+JiananYuan@users.noreply.github.com>
2022-01-12 11:58:58 +0800

    Update 最少交换次数来组合所有的1 II.cpp

--- 最少交换次数来组合所有的1 II.cpp
+++ 最少交换次数来组合所有的1 II.cpp
@@ -24,3 +24,26 @@ public:
         return ans;
     }
 };
+
+// Solution_2: 
+class Solution {
+    public int minSwaps(int[] nums) {
+        int len = nums.length, o = 0, res = Integer.MAX_VALUE;
+        //统计1的个数
+        for (int i : nums) {
+            o += i;
+        }
+        int l = 0, r = 0, count = 0;
+        //窗口左边界为0，将右边界移动到 o-1处，使窗口大小为o
+        while (r < o - 1){
+            count += nums[r++]; //计算窗口中1的个数
+        }
+        //因为是环形数组，所以得窗口左边界一直移动到数组末尾，才能结束
+        while (l < len){
+            count += nums[r++ % len];
+            res = Math.min(res, o - count); //o-count：窗口中0的个数
+            count -= nums[l++]; //左边界右移一位
+        }
+        return res;
+    }
+}
