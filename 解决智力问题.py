afad080c9d9a9f92a91daddfc723ff3ac617a7cd
Andy <53621620+JiananYuan@users.noreply.github.com>
2022-01-21 13:14:55 +0800

    Update 解决智力问题.py

--- 解决智力问题.py
+++ 解决智力问题.py
@@ -8,3 +8,22 @@ class Solution:
             return max(points+solve(t+brainpower+1), solve(t+1))
         
         return solve()
+
+    
+// dp：（C++）
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = int(questions.size());
+        vector<long long> dp(n + 1);
+        for (int i = 0; i < n; i += 1) {
+            // do or not do
+            dp[i + 1] = max(dp[i + 1], dp[i]);
+            auto& qus = questions[i];
+            int right_most = min(n, i + qus[1] + 1);
+            dp[right_most] = max(dp[i] + qus[0], dp[right_most]);
+        }
+        return dp[n];
+    }
+};
+
