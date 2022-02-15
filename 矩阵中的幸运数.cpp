b3d2db3bbc4a9314239efaa6b5da84846e9b5ef1
Andy <53621620+JiananYuan@users.noreply.github.com>
2022-02-15 22:40:02 +0800

    Create 矩阵中的幸运数.cpp

---
+++ 矩阵中的幸运数.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> res;
+        for (int i = 0; i < m; i += 1) {
+            int minx = matrix[i][0], minid = 0;
+            for (int j = 1; j < n; j += 1) {
+                if (matrix[i][j] < minx) {
+                    minx = matrix[i][j];
+                    minid = j;
+                }
+            }
+            bool need = true;
+            for (int j = 0; j < m; j += 1) {
+                if (minx < matrix[j][minid]) {
+                    need = false;
+                    break;
+                }
+            }
+            if (need) {
+                res.push_back(minx);
+            }
+        }
+        return res;
+    }
+};
