7e35ac60c3b73d09057d70f32e47bd591222b815
Andy <53621620+JiananYuan@users.noreply.github.com>
2022-02-18 21:29:28 +0800

    Create 找出星型图的中心节点.cpp

---
+++ 找出星型图的中心节点.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        unordered_map<int, int> ump;
+        for (vector<int>& edge : edges) {
+            int va = edge[0];
+            int vb = edge[1];
+            if (ump.count(va) == 0) {
+                ump[va] = 1;
+            }
+            else {
+                ump[va] += 1;
+            }
+            if (ump.count(vb) == 0) {
+                ump[vb] = 1;
+            }
+            else {
+                ump[vb] += 1;
+            }
+        }
+        int max_val = -1;
+        int max_id = 0;
+        for (auto& p : ump) {
+            if (p.second > max_val) {
+                max_val = p.second;
+                max_id = p.first;
+            }
+        }
+        return max_id;
+    }
+};
+
