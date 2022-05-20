3e7ab27a4ca4db28a314b1151232966bd004e5e0
Andy <53621620+JiananYuan@users.noreply.github.com>
2022-05-20 09:24:26 +0800

    Update 回文日期（蓝桥杯）.cpp

--- 回文日期（蓝桥杯）.cpp
+++ 回文日期（蓝桥杯）.cpp
@@ -1,72 +1,61 @@
 #include <iostream>
-#include <sstream>
+#include <algorithm>
 using namespace std;
 
-int day_of_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-string reverse_num(int num) {
-    int ans = 0;
-    while (num != 0) {
-        ans = ans * 10 + (num % 10);
-        num /= 10;
-    }
-    stringstream ss;
-    int tmp = ans;
-    int len = 0;
-    while (tmp != 0) {
-        tmp /= 10;
-        len += 1;
-    }
-    for (int i = 0; i < 4 - len; i += 1) {
-        ss << 0;
-    }
-    ss << ans;
-    return ss.str();
+inline bool is_run(int y) {
+  return (y % 400 == 0) || ((y % 4 == 0) && (y % 100 != 0));
 }
 
-bool is_ABAB(string num) {
-    return (num[0] == num[2]) && (num[1] == num[3]);
+string my_to_string(int d) {
+  string rec = "";
+  while (d) {
+    rec = char(d % 10 + '0') + rec;
+    d /= 10;
+  }
+  return rec;
+}
+
+inline int my_atoi(string str) {
+  int len = str.length();
+  int rec = 0;
+  for (int i = 0; i < len; i += 1) {
+    rec = rec * 10 + (str[i] - '0');
+  }
+  return rec;
 }
 
 int main() {
-    // 输入日期
-    int N;
-    cin >> N;
-    // 分离年份
-    int year = N / 10000;
-    // 存储答案
-    int ans_1 = 0, ans_2 = 0;
-    // 是否都已经找到
-    bool is_ans_1_found = false, is_ans_2_found = false;
-    // 以年份为粒度查找
-    for (int i = year; !is_ans_1_found || !is_ans_2_found; i += 1) {
-        // 得到月份和日期的表示格式 mm/dd
-        string after_year = reverse_num(i);
-        // 分离月份和日期
-        int month = (after_year[0] - '0') * 10 + (after_year[1] - '0');
-        int day =  (after_year[2] - '0') * 10 + (after_year[3] - '0');
-        if (month > 12) {
-            continue;
+  int date_1, date_2;
+  cin >> date_1 >> date_2;
+  string str_date_1 = my_to_string(date_1), str_date_2 = my_to_string(date_2);
+  int y_1 = date_1 / 10000, d_1 = date_1 % 100, m_1 = (date_1 - y_1 * 10000) / 100;
+  int y_2 = date_2 / 10000, d_2 = date_2 % 100, m_2 = (date_2 - y_2 * 10000) / 100;
+  // cout << y_1 << " " << m_1 << " " << d_1 << " " << y_2 << " " << m_2 << " " << d_2 << endl;
+  const int run[] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  const int non_run[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  int ans = 0;
+  for (int y = y_1; y <= y_2; y += 1) {
+    string tmp = my_to_string(y);
+    reverse(tmp.begin(), tmp.end());
+    string date = my_to_string(y) + tmp;
+    // cout << date << endl;
+    int m = my_atoi(date.substr(4, 2));
+    int d = my_atoi(date.substr(6, 2));
+    if (m >= 1 && m <= 12) {
+      if (date >= str_date_1 && date <= str_date_2) {
+        if (is_run(y)) {
+          if (d >= 1 && d <= run[m]) {
+            ans += 1;
+          }
         }
-        // 题目的限制，此处就可以不用再多做判断
-        if (day > day_of_month[month]) {
-            continue;
+        else {
+          if (d >= 1 && d <= non_run[m]) {
+            ans += 1;
+          }
         }
-        int ans = i * 10000 + month * 100 + day;
-        // 不可以比当前日期小
-        if (ans <= N) {
-            continue;
-        }
-        if (!is_ans_1_found) {
-            ans_1 = ans;
-            is_ans_1_found = true;
-        }
-        if (!is_ans_2_found && is_ABAB(after_year)) {
-            ans_2 = ans;
-            is_ans_2_found = true;
-        }
+      }
     }
-    cout << ans_1 << endl << ans_2 << endl;
-    return 0;
+  }
+  cout << ans << endl;
+  return 0;
 }
-
